ffc8773216add448b8a67015eabfb3f8123c58bc
yunus_emre <yunus.gkturk@gmail.com>
2023-10-03 22:23:25 +0300

    Feature selector added.

--- feature_selector.py
+++ feature_selector.py
@@ -9,3 +9,12 @@ def feature_selector(train, test, weights, threshold=0.1):
     :param threshold: Threshold for selecting features. It is 0.1 by default
     :return: Train and Test dataset with selected features.
     """
+
+    indices = weights >= threshold
+
+    print(indices)
+
+    new_train = train[:, indices]
+    new_test = test[indices]
+    print(new_train.shape)
+    print(new_test.shape)
