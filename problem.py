89efb96f479102e7185855a908d41e473d3d0d5a
yunus_emre <yunus.gkturk@gmail.com>
2023-10-05 23:03:12 +0300

    Docstrings added.

--- problem.py
+++ problem.py
@@ -2,4 +2,16 @@ from knn.knn import knn
 
 
 def problem(weights, X_train, y_train, X_test, y_test, k):
+    """
+    Calls Knn function to evaluate weights found by optimizer.
+
+    :param weights: Feature weights found by optimizer.
+    :param X_train: Train dataset
+    :param y_train: Target values for train.
+    :param X_test: Test dataset.
+    :param y_test: Target values for test.
+    :param k: Neighbor number.
+
+    :return: Miss classification error calculated by knn function.
+    """
     return knn(X_train, y_train, X_test, y_test, k, weights)
