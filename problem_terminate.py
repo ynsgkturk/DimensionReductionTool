9834bc3cf468d66ca87ce82b0e007ad56a51c648
yunus_emre <yunus.gkturk@gmail.com>
2023-10-05 22:59:50 +0300

    Docstrings added.

--- problem_terminate.py
+++ problem_terminate.py
@@ -2,9 +2,17 @@ import numpy as np
 
 
 def problem_terminate(dimension):
+    """
+    Problem specific parameter settings.
+    :param dimension: Dimension of search space.
+    :return: N: Particle number,
+    max_fe: Max fitness evaluation number,
+    lb: Lower bound array,
+    ub: Upper bound array
+    """
     n = 25
-    max_FE = 300
+    max_fe = 300
     lb = np.zeros((1, dimension))
     ub = np.ones((1, dimension))
 
-    return n, max_FE, lb, ub
+    return n, max_fe, lb, ub
