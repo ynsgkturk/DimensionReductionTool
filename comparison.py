4a24f1dc95d76dcdc7c54f270c8b718c91262f94
yunus_emre <yunus.gkturk@gmail.com>
2023-11-17 21:48:07 +0300

    Code for comparing different machine learning models.

--- comparison.py
+++ comparison.py
@@ -11,6 +11,7 @@ from sklearn.ensemble import RandomForestClassifier, HistGradientBoostingClassif
 
 from sklearn.metrics import accuracy_score
 import pandas as pd
+from scipy.io import loadmat
 
 from feature_selector import feature_selector
 from knn.knn import knn
@@ -19,61 +20,77 @@ from problem import problem
 from problem_terminate import problem_terminate
 from read_data import read_data
 
+import warnings
+
 # Variables
 models = {
     'xgb': XGBClassifier(random_state=42, objective='accuracy'),
     'knn': knn,
-    'lgbm': LGBMClassifier(random_state=42, objective='accuracy'),
+    'lgbm': LGBMClassifier(random_state=42, objective='multiclass', verbose=-1),
     'rf': RandomForestClassifier(random_state=42),
     'hgb': HistGradientBoostingClassifier(random_state=42),
 }
 
 
 def get_model_accuracy(models, train, test):
-    X_train, y_train = train
-    X_test, y_test = test
+    x_train, y_train = train
+    x_test, y_test = test
 
     scores = {}
 
     for label, model in models.items():
         if not label == 'knn':
-            model.fit(X_train, y_train)
+            model.fit(x_train, y_train)
 
-            y_pred = model.predict(X_test)
+            y_pred = model.predict(x_test)
             score = accuracy_score(y_test, y_pred)
 
             scores[label] = score
         else:
-            error_rate = model(X_train, y_train, X_test, y_test, 5)
+            error_rate = model(x_train, y_train, x_test, y_test, 5)
             scores[label] = 1-error_rate
 
     return scores
 
 
 def main():
+    warnings.filterwarnings('ignore')
+
     train_path = "data\\urban_land_cover\\train.csv"
     test_path = "data\\urban_land_cover\\test.csv"
-    X_train, y_train, X_test, y_test = read_data(train_path, test_path)
+    x_train, y_train, x_test, y_test = read_data(train_path, test_path)
 
     # Get scores before dimension reduction
-    scores = get_model_accuracy(models, (X_train, y_train), (X_test, y_test))
+    scores = get_model_accuracy(models, (x_train, y_train), (x_test, y_test))
 
-    g_best, history = pso(5, problem, problem_terminate, X_train, y_train, X_test, y_test)
+    # PSO
+    g_best, history = pso(5, problem, problem_terminate, x_train, y_train, x_test, y_test)
 
     # Dimension reduction
-    new_train, new_test = feature_selector(X_train, X_test, g_best['weights'][0])
+    new_train, new_test = feature_selector(x_train, x_test, g_best['weights'][0])
     print(new_train.shape, new_test.shape)
 
     # Get scores after dimension reduction
-    scores_after = get_model_accuracy(models, (new_train, y_train),
+    scores_pso = get_model_accuracy(models, (new_train, y_train),
                                       (new_test, y_test))
 
-    scores = pd.DataFrame(scores)
-    scores_after = pd.DataFrame(scores_after)
+    # FDB-AOA
+    fdb_aoa_weights = loadmat(r'weights\weights.mat')
+
+    new_train, new_test = feature_selector(x_train, x_test, fdb_aoa_weights['bestWeights'][0])
+    print(new_train.shape, new_test.shape)
+
+    # Get scores after dimension reduction
+    scores_aoa = get_model_accuracy(models, (new_train, y_train),
+                                    (new_test, y_test))
+
+    scores = pd.DataFrame({'model': scores.keys(),
+                           'score': scores.values() ,
+                           'pso_score': scores_pso.values(),
+                           'fdb_aoa_score': scores_aoa.values()})
 
     with pd.ExcelWriter('results.xlsx', engine='xlsxwriter') as writer:
         scores.to_excel(writer, sheet_name='Scores', index=False)
-        scores_after.to_excel(writer, sheet_name='Scores_after', index=False)
 
 
 
